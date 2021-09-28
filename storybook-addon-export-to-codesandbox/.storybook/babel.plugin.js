ec1904955c1bfe6fca93487ae0e8de675c7c1709
Peter Dräxler <peter@draxler.ml>
2021-09-28 17:14:35 +0200

    update babel plugin description

--- storybook-addon-export-to-codesandbox/.storybook/babel.plugin.js
+++ storybook-addon-export-to-codesandbox/.storybook/babel.plugin.js
@@ -1,7 +1,16 @@
 /**
  * This Babel plugin adds `context.parameters.fullSource` property to Storybook stories,
  * which contains source of of the file where story is present.
- * It’s utilized by Export to CodeSandbox.
+ * 
+ * Specifically, it finds this expression in a story file: storyName.parameters = ...
+ * And adds the following expression after it: storyName.parameters.fullSource = __STORY__;
+ * 
+ * The __STORY__ variable is added to each story file by 
+ * [Storybook’s webpack loader](https://github.com/storybookjs/storybook/tree/next/lib/source-loader)
+ * and contains source code of the story file.
+ * 
+ * This plugin is utilized by Export to CodeSandbox.
+ * 
  * @param {import('@babel/core')} babel
  * @returns {import('@babel/core').PluginObj}
  */
