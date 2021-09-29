5788f1d9a531c89d7747c3c8a6fcdd95cf54454b
Peter Dräxler <peter@draxler.ml>
2021-09-29 13:24:40 +0200

    export babel plugin

--- storybook-addon-export-to-codesandbox/src/index.ts
+++ storybook-addon-export-to-codesandbox/src/index.ts
@@ -1,6 +1,7 @@
+export { default as babelPlugin } from "../.storybook/babel.plugin";
+
 if (module && module.hot && module.hot.decline) {
   module.hot.decline();
 }
-
 // make it work with --isolatedModules
 export default {};
