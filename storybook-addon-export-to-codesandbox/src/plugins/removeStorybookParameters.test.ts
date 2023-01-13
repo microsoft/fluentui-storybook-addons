ea5bd88382a96e36fead9432bfc54627b68fdce1
Jiří Vyhnálek <jirivyhnalek@microsoft.com>
2023-01-13 11:56:06 +0100

    add tests

---
+++ storybook-addon-export-to-codesandbox/src/plugins/removeStorybookParameters.test.ts
@@ -0,0 +1,18 @@
+import pluginTester from 'babel-plugin-tester';
+import * as path from 'path';
+import plugin, { PLUGIN_NAME } from './removeStorybookParameters';
+
+const fixturesDir = path.join(__dirname, `__fixtures__/${PLUGIN_NAME}`);
+
+const defaultDependencyReplace = { replace: '@fluentui/react-components' };
+
+pluginTester({
+  pluginOptions: {
+    '@fluentui/react-button': defaultDependencyReplace,
+    '@fluentui/react-menu': defaultDependencyReplace,
+    '@fluentui/react-link': defaultDependencyReplace,
+  },
+  pluginName: PLUGIN_NAME,
+  plugin,
+  fixtures: fixturesDir,
+});
